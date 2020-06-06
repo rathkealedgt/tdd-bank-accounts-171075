ba8d51b956acb6a6ea2fda189bb9ff76a05bfe47
BenJohn <171075@trinityschools.nz>
2020-06-07 11:07:57 +1200

    Revert "Merge branch 'master' of https://github.com/rathkealedgt/tdd-bank-accounts-171075"
    This reverts commit 9692e353de502dfc0d571640f705bcf964bcc116, reversing
    changes made to 16a0e19cbc72b2edec5c0e9a1c798ea2f708c9ae.

---
+++ BankAccountTest/BankAccounts/BankAccount.vb
@@ -0,0 +1,117 @@
+﻿Public Class BankAccount
+
+    Private accountHolder As String
+    Private accountNumber As String
+    Private balance As Double
+    Private interestRate As Double
+    Private countryOfOrigin As String
+
+
+    Public Sub New(accountHolderInput As String, accountNumberInput As String, balanceInput As Double, interestRateInput As Double, countryOfOriginInput As String)
+
+        Me.accountHolder = accountHolderInput
+        Me.accountNumber = accountNumberInput
+        Me.balance = balanceInput
+        Me.interestRate = interestRateInput
+        Me.countryOfOrigin = countryOfOriginInput
+
+    End Sub
+    Public Sub New(accountHolderInput As String, accountNumberInput As String, balanceInput As Double, interestRateInput As Double)
+
+        Me.accountHolder = accountHolderInput
+        Me.accountNumber = accountNumberInput
+        Me.balance = balanceInput
+        Me.interestRate = interestRateInput
+
+    End Sub
+    Public Sub New(accountHolderInput As String, accountNumberInput As String, balanceInput As Double)
+
+        Me.accountHolder = accountHolderInput
+        Me.accountNumber = accountNumberInput
+        Me.balance = balanceInput
+
+    End Sub
+
+    Function GetAccountNumber()
+
+        Return Me.accountNumber
+
+    End Function
+
+    Function GetAccountHolder()
+
+        Return Me.accountHolder
+
+    End Function
+
+    Function GetBalance()
+
+        Return Me.balance
+
+    End Function
+
+    Function GetInterestRate()
+
+        Return Me.interestRate
+
+    End Function
+
+    Function GetCountryOfOrigin()
+
+        Return Me.countryOfOrigin
+
+    End Function
+
+    Function SetInterestRate(interestRateInput As Double)
+
+        Me.interestRate = interestRateInput
+
+        Return Nothing
+
+    End Function
+
+    Function Deposit(depositInput As Double)
+
+        Me.balance = Me.balance + depositInput
+
+        Return Me.balance
+
+    End Function
+
+    Function Withdraw(withdrawInput As Double)
+
+        Dim newBalance As Double
+
+        newBalance = Me.balance - withdrawInput
+
+        If newBalance <= 0 Then
+
+            Return Me.balance
+
+        Else
+
+            Me.balance = newBalance
+
+            Return Me.balance
+
+
+        End If
+
+    End Function
+
+    Function ApplyInterestRate()
+
+        Me.balance = Me.balance * ((Me.interestRate) * (1 / 12) + 1)
+
+        Return Me.balance
+
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return (Me.accountHolder & vbCrLf &
+                 Me.accountNumber & vbCrLf &
+                 Me.countryOfOrigin & vbCrLf &
+                 Me.balance & vbCrLf &
+                 Me.interestRate * 100 & "%")
+    End Function
+End Class
--- BankAccounts/Module1.vb
+++ BankAccounts/Module1.vb
@@ -1,26 +1,12 @@
-﻿Imports System.Windows.Forms
-Imports System.Drawing
-Module Module1
-
+﻿Module Module1
 
     Sub Main()
 
-        Application.Run(New MainInterface())
+        Dim accountone As New BankAccount("Henry", "GRFI 125155 12565342", 134135.31, 0.03, "New Zealand")
 
+        Console.WriteLine(accountone.ToString)
 
-        'Dim accountone As New BankAccount("Henry", "GRFI 125155 12565342", 134135.31, 0.03, "New Zealand")
-
-        'Console.WriteLine(accountone.ToString)
-
-        'Console.ReadLine()
-
-        'Console.WriteLine("Please enter a withdraw amount")
-        'Dim withdrawInput As Double = Console.ReadLine()
-
-        'accountone.Withdraw(withdrawInput)
-
-        'Console.WriteLine(accountone.ToString)
-        'Console.ReadLine()
+        Console.ReadLine()
 
     End Sub
 
